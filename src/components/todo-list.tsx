2235ef7dab8d1b19b47a6afaf25054b26d6d616e
Santiago Luis Ocón <ocon.santiago@gmail.com>
2023-11-09 15:10:17 -0300

    Refactor TodoList component to use ternary operator instead of if/else statement.

--- src/components/todo-list.tsx
+++ src/components/todo-list.tsx
@@ -7,20 +7,17 @@ type Props = {
 };
 
 export default function TodoList({ todos, setTodos }: Props) {
-  let content;
-  if (todos.length === 0) {
-    content = <p>No Todos Available</p>;
-  } else {
-    const sortedTodos = todos.sort((a, b) => b.id - a.id);
-
-    content = (
-      <>
-        {sortedTodos.map((todo) => (
-          <TodoItem key={todo.id} todo={todo} setTodos={setTodos} />
-        ))}
-      </>
-    );
-  }
-
-  return content;
+  return (
+    <>
+      {todos.length === 0 ? (
+        <p>No Todos Available</p>
+      ) : (
+        <>
+          {todos.map((todo) => (
+            <TodoItem key={todo.id} todo={todo} setTodos={setTodos} />
+          ))}
+        </>
+      )}
+    </>
+  );
 }
